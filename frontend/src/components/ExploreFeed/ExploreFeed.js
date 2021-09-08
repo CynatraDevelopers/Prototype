6eefa0580ad21f1fcf63ac9c4247301a6b6e7e3a
LakshmiNarayanP <lakshminarayanpaidisetty@gmail.com>
2021-09-08 20:45:56 +0530

    Explore feed basics added

---
+++ frontend/src/components/ExploreFeed/ExploreFeed.js
@@ -0,0 +1,30 @@
+import './ExploreFeed.css'
+
+function Explore() {
+    return(
+        <div className="container">
+                <div className="gallery-item w-2 h-2">
+                    <div class="image"> 
+                        <img src="https://source.unsplash.com/random" alt="digital-art"></img> 
+                    </div>
+                </div>
+                <div className="gallery-item w-1 h-3">
+                    <div class="image"> 
+                        <img src="https://source.unsplash.com/random" alt="digital-art"></img> 
+                    </div>
+                </div>
+                <div className="gallery-item w-3 h-1">
+                    <div class="image"> 
+                        <img src="https://source.unsplash.com/random" alt="digital-art"></img> 
+                    </div>
+                </div>
+                <div className="gallery-item">
+                    <div class="image"> 
+                        <img src="https://source.unsplash.com/random" alt="digital-art"></img> 
+                    </div>
+                </div>
+        </div>
+    );
+}
+
+export default Explore;
