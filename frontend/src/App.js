6ebf4ab260db8d65d84ec35fd80668309e5dc5f1
ammrit2312 <amriteshc101@icloud.com>
2021-09-11 10:18:02 +0530

    Added files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 import LoginForm from './components/Login/Login';
 import SignupForm from './components/Signup/Signup';
 import Profile from './components/Profile/Profile';
+import NavigationBar from './components/Navbar/Navbar';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' exact component={LoginForm} />
         <Route path='/signup' exact component={SignupForm} />
         <Route path='/profile' exact component={Profile} />
+        <Route path='/explore' exact component={NavigationBar} />
       </Switch>
     </Router>
   );
