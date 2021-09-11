4538b414ab96597363d0bfb6a8dd3a224592c912
LakshmiNarayanP <lakshminarayanpaidisetty@gmail.com>
2021-09-11 14:29:44 +0530

    Resized explore feed on /explore page (1)

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -5,26 +5,28 @@ import Explore from '../ExploreFeed/ExploreFeed';
 
 function NavigationBar() {
     return (
-      <div className="NavBar">
-        <div className="Rect">
-          <div className="Circle">
-            <p className="AccName">AccountName</p>
-          </div>
-          <div>
-            <FaHome className="iconHome"/>
-            <p className="HomeText">Home</p>
-          </div>
-          <div>
-            <FaBell className="iconBell"/>
-            <p className="BellText">Notifications</p>
-          </div>
-          <div>
-            <FaShareAlt className="iconShare"/>
-            <p className="ShareText">Share</p>
-          </div>
-          <div>
-            <FiSettings className="iconSettings"/>
-            <p className="SettingsText">Settings</p>
+      <div className="Explore_page">
+        <div className="NavBar">
+          <div className="Rect">
+            <div className="Circle">
+              <p className="AccName">AccountName</p>
+            </div>
+            <div>
+              <FaHome className="iconHome"/>
+              <p className="HomeText">Home</p>
+            </div>
+            <div>
+              <FaBell className="iconBell"/>
+              <p className="BellText">Notifications</p>
+            </div>
+            <div>
+              <FaShareAlt className="iconShare"/>
+              <p className="ShareText">Share</p>
+            </div>
+            <div>
+              <FiSettings className="iconSettings"/>
+              <p className="SettingsText">Settings</p>
+            </div>
           </div>
         </div>
         <Explore></Explore>
