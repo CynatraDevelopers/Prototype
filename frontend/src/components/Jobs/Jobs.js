ba31c236c1cfde94e57e92fbb8924210f6329a38
LakshmiNarayanP <lakshminarayanpaidisetty@gmail.com>
2021-09-24 19:55:56 +0530

    Completed jobs page

--- frontend/src/components/Jobs/Jobs.js
+++ frontend/src/components/Jobs/Jobs.js
@@ -1,7 +1,47 @@
 import './Jobs.css'
+import { TextField } from '@mui/material';
+import { FaHome, FaBell, FaShareAlt } from 'react-icons/fa';
+import { FiSettings } from 'react-icons/fi'
 
 const JobFeed = () => {
+    const submitSearch = (e) => {
+        e.preventDefault();
+    }
     return(
+        <div>
+            <div className="jobHeader">
+                {/* <div className="Title">
+                    {"C Y N A T R A"}
+                </div> */}
+                <div className="Search">
+                    <form>
+                        <TextField 
+                                id="outlined-basic" 
+                                className="Searchtext" 
+                                label="Search" 
+                                variant="outlined"
+                        />
+                        <button className="search__div" onClick={(e)=>submitSearch(e)}>
+                            <i class="fas fa-search explore__search"></i>
+                        </button>
+                    </form>
+                </div>
+                <div className="NavBarJobs">
+                    <FaHome className = "iconHomeJobs"/>
+                    <FaBell className = "iconBellJobs"/>
+                    <FaShareAlt className = "iconShareJobs"/>
+                    <FiSettings className = "iconSettingsJobs"/>
+                </div> 
+                <div className = "ProfileInfo">
+                    <div className = "RectCareer">
+                        <p className="CareerText">Career</p>
+                    </div>
+                    <div className = "RectName">
+                        <p className = "Username">Jelly</p>
+                    </div>
+                    <div className = "ProfilePic"/>
+                </div>
+            </div>
         <div className="jobBody">
             <div className="jobBodyHeader">
                 <p className="jobBodyHeaderText">Explore Opportunities</p>
@@ -15,60 +55,12 @@ const JobFeed = () => {
             </div>
 {/* RECT BOXES -------------------------------------------------------------------------------------------------------------- */}
 
-            <div className="jobRect">
-                <p className="jobName">Sony</p>
-                <div className="applyButton">
-                    <p className="applyText">Apply</p>
-                </div>
-            </div>
-
-            <div className="jobRect">
-                <p className="jobName">Sony</p>
-                <div className="applyButton">
-                    <p className="applyText">Apply</p>
-                </div>
-            </div>
-
-            <div className="jobRect">
-                <p className="jobName">Sony</p>
-                <div className="applyButton">
-                    <p className="applyText">Apply</p>
-                </div>
-            </div>
-
-            <div className="jobRect">
-                <p className="jobName">Sony</p>
-                <div className="applyButton">
-                    <p className="applyText">Apply</p>
-                </div>
-            </div>
-
-            <div className="jobRect">
-                <p className="jobName">Sony</p>
-                <div className="applyButton">
-                    <p className="applyText">Apply</p>
-                </div>
-            </div>
-
-            <div className="jobRect">
-                <p className="jobName">Sony</p>
-                <div className="applyButton">
-                    <p className="applyText">Apply</p>
-                </div>
-            </div>
-            <div className="jobRect">
-                <p className="jobName">Sony</p>
-                <div className="applyButton">
-                    <p className="applyText">Apply</p>
-                </div>
-            </div>
-
-
 {/* RECT BOXES -------------------------------------------------------------------------------------------------------------- */}
 
             <div className="messagingRect">
                 <p className="messagingRectText">Messaging</p>
             </div>
+        </div>
         </div>
     );
 
